25a873c0016903376f2c0bb2ad8995426e202802
Alister <Alister.moral@gmail.com>
2023-12-15 11:34:16 +0100

    starting the home page and componant

---
+++ src/Conposant/App.js
@@ -0,0 +1,19 @@
+import '../App.css';
+import Header from '../Conposant/Header';
+import Falaise from '../Conposant/Image';
+import Location from '../Conposant/Location';
+import Footer from '../Conposant/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Falaise/> 
+      <Location/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Conposant/Location.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../styles/Location.css';
+
+const Location = () => {
+  return (
+    <div className="location-container">
+      <article className='location-card'>
+          <div className='card-content'>Titre de la location</div>
+      </article>
+
+      <article className='location-card'>
+          <div className='card-content'>Titre de la location</div>
+      </article>
+
+      <article className='location-card'>
+          <div className='card-content'>Titre de la location</div>
+      </article>
+
+      <article className='location-card'>
+          <div className='card-content'>Titre de la location</div>
+      </article>
+
+      <article className='location-card'>
+          <div className='card-content'>Titre de la location</div>
+      </article>
+
+      <article className='location-card'>
+          <div className='card-content'>Titre de la location</div>
+      </article>
+    </div>
+  );
+};
+
+export default Location;
